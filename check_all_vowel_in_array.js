5eda64441fbe76705f518c86b091deaf6ca90153
sbachr <sbachr0501@gmail.com>
2018-07-11 20:10:52 +0700

    revisi penambahan example board

--- check_all_vowel_in_array.js
+++ check_all_vowel_in_array.js
@@ -1,31 +1,37 @@
 let generateBoard = function (row, col) {
     const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    let result = []
+    let count = []
     for (let i = 0; i < row; i++) {
         let temp = []
         for (let j = 0; j < col; j++) {
             let index = (Math.floor(Math.random() * 26));
             temp.push(alpha[index])
         }
-        result.push(temp)
+        count.push(temp)
     }
-    return result
+    return count
 }
 
+// let board = [
+//              ['A','X','C','Y'],
+//              ['E','O','O','S'],
+//              ['I','U','I','N'],
+//              ['M','Y','O','E'],
+//              ['P','D','A','I']
+//             ]
+            
 let randomAlphabet = function (board) {
     const alpha = 'AIUEO'
-    let result = 0
     let count = 0
     for (let i = 0; i < board.length-1; i++) {
         for (let j = 0; j < board[i].length - 1; j++) {
             count = 0
             for (let k = 0; k < alpha.length; k++) {
-                if(board[i][j] == alpha[k] && board[i][j+1] == alpha[k] && board[i+1][i] == alpha[k] && board[i+1][j+1] == alpha[k]) count++
+                if(board[i][j] == alpha[k] || board[i][j+1] == alpha[k] || board[i+1][i] == alpha[k] || board[i+1][j+1] == alpha[k]) count++
             }
-            result += count
         }
     }
-    return (result != 0 ? `You found ${result} Vowel` : `Vowel Not Found`)
+    return (count != 0 ? `You found ${count} Vowel` : `Vowel Not Found`)
 }
 
 
@@ -33,3 +39,4 @@ let dataInBoard = generateBoard(3, 5)
 
 console.log(dataInBoard)
 console.log(randomAlphabet(dataInBoard))
+
